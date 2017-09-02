d9b31d7b27c11ea95bda51b285414e512ae8dfb9
jpetryk <jasiek.petryk@gmail.com>
2017-09-02 15:10:06 +0200

    added nonnullable fields

--- src/main/kotlin/pl/jpetryk/kotlinshortener/domain/Link.kt
+++ src/main/kotlin/pl/jpetryk/kotlinshortener/domain/Link.kt
@@ -6,7 +6,8 @@ import javax.persistence.*
 data class Link(
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Long = 0,
+        @Column(nullable = false)
         val originalUrl: String = "",
-        @Column(unique = true)
+        @Column(unique = true, nullable = false)
         val redirectHash: String = ""
 )
